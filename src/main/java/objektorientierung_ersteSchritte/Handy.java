b532a3725c898e942286cc44223e41119d737252
TimoWolle <timosch3@gmx.de>
2023-03-10 10:34:38 +0100

    Handy // Hotfix

--- src/main/java/objektorientierung_ersteSchritte/Handy.java
+++ src/main/java/objektorientierung_ersteSchritte/Handy.java
@@ -1,4 +1,21 @@
 package objektorientierung_ersteSchritte;
 
 public class Handy {
+
+    public static int NewArtNR = 1;
+
+    private int ArtNR;
+
+
+    public Handy() {
+        this.ArtNR  = NewArtNR++;
+    }
+
+    public int getArtNR() {
+        return ArtNR;
+    }
+
+    public void setArtNR(int artNR) {
+        ArtNR = artNR;
+    }
 }
