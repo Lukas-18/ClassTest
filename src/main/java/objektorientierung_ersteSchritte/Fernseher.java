206bda6c493136733fe7ed95d8741b91e8aa9afa
TimoWolle <timosch3@gmx.de>
2023-03-10 10:28:05 +0100

    Fernsehr // Bugfix

--- src/main/java/objektorientierung_ersteSchritte/Fernseher.java
+++ src/main/java/objektorientierung_ersteSchritte/Fernseher.java
@@ -1,4 +1,69 @@
 package objektorientierung_ersteSchritte;
 
 public class Fernseher {
+
+    private boolean istAn = false;
+    private int aktuellerSender;
+    private int lautstärke;
+
+    //region Region.Konstruktor
+
+    public Fernseher() {
+    }
+
+    public Fernseher(boolean istAn, short aktuellerSender, int lautstärke) {
+        this.istAn = istAn;
+        this.aktuellerSender = aktuellerSender;
+        this.lautstärke = lautstärke;
+    }
+
+    //endregion
+
+    //region Region:Funktionen
+
+    public void lautstärkeÄndern(int _difference){
+        this.lautstärke =+ _difference;
+    }
+
+    public void umschalten(int _difference){
+        this.aktuellerSender =+ _difference;
+    }
+
+    public void schalteAn(){
+        this.istAn = true;
+    }
+
+    public void schalteAus(){
+        this.istAn = false;
+    }
+
+    //endregion
+
+    //region Region:Getter/Setter
+
+    public boolean isIstAn() {
+        return istAn;
+    }
+
+    public void setIstAn(boolean istAn) {
+        this.istAn = istAn;
+    }
+
+    public int getLautstärke() {
+        return lautstärke;
+    }
+
+    public void setLautstärke(int lautstärke) {
+        this.lautstärke = lautstärke;
+    }
+
+    public int getAktuellerSender() {
+        return aktuellerSender;
+    }
+
+    public void setAktuellerSender(int aktuellerSender) {
+        this.aktuellerSender = aktuellerSender;
+    }
+
+    //endregion
 }
