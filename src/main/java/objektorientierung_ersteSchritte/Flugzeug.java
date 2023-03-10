e2f2e96a53eaba8009fe6013f8141549002b06e5
TimoWolle <timosch3@gmx.de>
2023-03-10 09:15:14 +0100

    Getter / Setter

--- src/main/java/objektorientierung_ersteSchritte/Flugzeug.java
+++ src/main/java/objektorientierung_ersteSchritte/Flugzeug.java
@@ -1,4 +1,60 @@
 package objektorientierung_ersteSchritte;
 
+import javax.swing.plaf.synth.Region;
+
 public class Flugzeug {
+    private int sitzplaetze;
+    private String flugzeugmarke;
+    private String reiseziel;
+    private String ausgangsLand;
+
+
+    public Flugzeug() {
+    }
+
+    public Flugzeug(String _name, String _ausgangsLand){
+        this.flugzeugmarke = _name;
+        this.ausgangsLand = _ausgangsLand;
+    }
+    
+    public void fliegen(){
+
+    }
+
+//region Region:Getter/Setter
+
+    public String getFlugzeugmarke() {
+        return flugzeugmarke;
+    }
+
+    public void setFlugzeugmarke(String flugzeugmarke) {
+        this.flugzeugmarke = flugzeugmarke;
+    }
+
+    public String getReiseziel() {
+        return reiseziel;
+    }
+
+    public void setReiseziel(String reiseziel) {
+        this.reiseziel = reiseziel;
+    }
+
+    public String getAusgangsLand() {
+        return ausgangsLand;
+    }
+
+    public void setAusgangsLand(String ausgangsLand) {
+        this.ausgangsLand = ausgangsLand;
+    }
+
+    public int getSitzplaetze(){
+        return sitzplaetze;
+    }
+
+    public void setSitzplaetze(int sitzplaetze){
+        this.sitzplaetze = sitzplaetze;
+    }
+
+//endregion
+
 }
